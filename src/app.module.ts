9d9af9de3b9079606f427d5bf1c3bf989340d5b2
Vipul Khanna <khannavipul06@gmail.com>
2025-01-26 16:19:01 +0530

    fix:smtp client

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,6 @@ import * as autoPopulate from 'mongoose-autopopulate';
 import { LoggerMiddleware } from './common/utils/logger';
 import { StripeModule } from './stripe/stripe.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
-export const SMTP_SERVICE_PROVIDERS = {
-  gmail: 'GMAIL',
-  mailGun: 'MAIL_GUN',
-};
-
 @Module({
   imports: [
     AuthModule,
@@ -29,38 +24,6 @@ export const SMTP_SERVICE_PROVIDERS = {
         DATABASE_URL: Joi.string().uri().required(),
         JWT_SECRET: Joi.string().required(),
         STRIPE_API_KEY: Joi.string().required(),
-        SMTP_CLIENT: Joi.string()
-          .default(SMTP_SERVICE_PROVIDERS.gmail)
-          .custom((value, helpers) => {
-            if (!Object.values(SMTP_SERVICE_PROVIDERS).includes(value)) {
-              return helpers.error('any.invalid', { value });
-            }
-
-            // Additional validation for MAIL_GUN
-            if (value === SMTP_SERVICE_PROVIDERS.mailGun) {
-              const requiredVars = [
-                { key: 'MAILGUN_API_KEY', value: process.env.MAILGUN_API_KEY },
-                { key: 'MAILGUN_DOMAIN', value: process.env.MAILGUN_DOMAIN },
-                { key: 'MAILGUN_HOST', value: process.env.MAILGUN_HOST },
-              ];
-
-              const missingVars = requiredVars.filter((env) => !env.value);
-
-              if (missingVars.length > 0) {
-                return helpers.error('mailgun.missingVars', {
-                  value: missingVars.map((env) => env.key).join(', '),
-                });
-              }
-            }
-
-            return value; // Validation passed
-          }, 'Custom validation')
-          .messages({
-            'any.invalid': 'The value must be either GMAIL or MAIL_GUN.',
-            'mailgun.missingVars':
-              'Missing required environment variables for MAIL_GUN: {{#value}}',
-            'string.base': 'The value must be a string.',
-          }),
       }),
     }),
     MongooseModule.forRootAsync({
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,39 +1,16 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { sendOTP } from 'src/type/user.entity';
-import Mailgun from 'mailgun.js';
 import { IMailgunClient } from 'mailgun.js/Interfaces';
-import { SMTP_SERVICE_PROVIDERS } from 'src/app.module';
 
 @Injectable()
 export class MailService {
-  private readonly mailgun: Mailgun;
   private readonly mailGunClient: IMailgunClient;
   private readonly activeMailingService: string;
   private readonly MAILGUN_DOMAIN: string;
-  constructor(
-    private readonly configService: ConfigService,
-    private mailerService: MailerService,
-  ) {
-    this.activeMailingService = configService.get('SMTP_CLIENT');
-
-    if (this.activeMailingService == SMTP_SERVICE_PROVIDERS.mailGun) {
-      this.mailgun = new Mailgun(FormData);
-      this.mailGunClient = this.mailgun.client({
-        key: this.configService.get('MAILGUN_API_KEY'),
-        username: 'api',
-        url: this.configService.get('MAILGUN_HOST'),
-      });
-      this.MAILGUN_DOMAIN = this.configService.get<string>('MAILGUN_DOMAIN');
-    }
-  }
+  constructor(private mailerService: MailerService) {}
   async sendMail(data) {
-    if (this.activeMailingService == SMTP_SERVICE_PROVIDERS.mailGun) {
-      await this.mailGunClient.messages.create(this.MAILGUN_DOMAIN, data);
-    } else {
-      await this.mailerService.sendMail(data);
-    }
+    await this.mailerService.sendMail(data);
   }
 
   async sendUserConfirmation(user: sendOTP) {
