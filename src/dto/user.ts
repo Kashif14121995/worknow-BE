5dd91299e8e7b29610efb6e7c6509d34a44c4146
Vipul Khanna <khannavipul06@gmail.com>
2025-02-01 23:46:38 +0530

    fix:remove password regex

--- src/dto/user.ts
+++ src/dto/user.ts
@@ -29,9 +29,9 @@ export class CreateUserDto {
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
   @MaxLength(20, { message: 'Password must not exceed 20 characters' })
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, {
-    message: 'password must contain at least one letter and one number',
-  })
+  // @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, {
+  //   message: 'password must contain at least one letter and one number',
+  // })
   password: string;
 
   @IsEnum([UserRole.job_provider, UserRole.job_seeker], {
