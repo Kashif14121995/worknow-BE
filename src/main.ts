a16bbc903c417d92b0c2f216d4ab747ec9af23a2
Mohammad Saleem <saleem27898@gmail.com>
2025-07-19 21:03:28 +0530

    Updated API docs path

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,31 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from './swagger.json';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  app
-    .useGlobalPipes(
-      new ValidationPipe({
-        transform: true,
-      }),
-    )
-    .enableCors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+  // 1. Global prefix must be set before swagger
   app.setGlobalPrefix('api/v1');
 
+  // 2. Middlewares
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // 3. Swagger route adjusted to global prefix
+  app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Application is running...`);
 }
 bootstrap();
